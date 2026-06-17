3eb3bd4a9b293d4a250467bc32a93d585a439535
PrajanBasnet <spiderbasnet140@gmail.com>
2026-06-17 07:55:47 +0545

    fix: db

--- db/db.js
+++ db/db.js
@@ -8,12 +8,6 @@ const pool = new Pool({
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORT,
-//   });
+
 
   module.exports = pool;
